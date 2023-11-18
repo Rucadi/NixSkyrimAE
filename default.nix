fc376c75f922ff8ebc6ca70f4752ab655a8e5ad1
rucadi <ruben.cano96@gmail.com>
2023-11-18 22:22:34 +0100

    test

--- default.nix
+++ default.nix
@@ -1,3 +1,3 @@
 {system, pkgs, nexus_mods_cookie, modlist_name}:
-import ./mods/all-mods.nix {inherit system nexus_mods_cookie pkgs; modlist_name="AAA"}
+import ./mods/all-mods.nix {inherit system nexus_mods_cookie pkgs; modlist_name="AAA";}
 
