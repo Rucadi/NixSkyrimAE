46f7a4cbefd06d416b2f5d580723828773e8e653
rucadi <ruben.cano96@gmail.com>
2023-11-18 22:30:59 +0100

    test

--- mods/all-mods.nix
+++ mods/all-mods.nix
@@ -1,13 +1,15 @@
 {pkgs, modlist_name, ...}:
 rec {
-    inherit modlist_name;
-    defaultModBuilder = pkgs.callPackage ./nixutils/builder/defaultModBuilder.nix {};
-
-    USSEP = pkgs.callPackage ./bugfixes/USSEP {};
     
-    SKYRIM202X_1 = pkgs.callPackage ./ModelsAndTextures/SKYRIM202X_part1 { };
-    SKYRIM202X_2 = pkgs.callPackage ./ModelsAndTextures/SKYRIM202X_part2 { };
-    SKYRIM202X_3 = pkgs.callPackage ./ModelsAndTextures/SKYRIM202X_part3 { };
+    callPackage = lib.callPackageWith (modlist_name);
+
+    defaultModBuilder = callPackage ./nixutils/builder/defaultModBuilder.nix {};
+
+    USSEP = callPackage ./bugfixes/USSEP {};
+    
+    SKYRIM202X_1 = callPackage ./ModelsAndTextures/SKYRIM202X_part1 { };
+    SKYRIM202X_2 = callPackage ./ModelsAndTextures/SKYRIM202X_part2 { };
+    SKYRIM202X_3 = callPackage ./ModelsAndTextures/SKYRIM202X_part3 { };
     
     SKYRIM202X = pkgs.buildEnv rec {
                         name = "SKYRIM202X";
@@ -19,6 +21,6 @@ rec {
                 ignoreCollisions= true;
             };
     
-    SKSE = pkgs.callPackage ./SKSE {};
-    RaceMenu = pkgs.callPackage ./UserInterface/RaceMenu {};
+    SKSE = callPackage ./SKSE {};
+    RaceMenu = callPackage ./UserInterface/RaceMenu {};
 }
